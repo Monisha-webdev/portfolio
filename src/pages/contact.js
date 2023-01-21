c2d8c7bd5e4c2b5f94b524ab60706cdb18b55031
monisha-git <monishainfo999@gmail.com>
2023-01-22 00:43:17 +0530

    contact resume and edex page completed

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,13 +5,59 @@ import "bootstrap/dist/css/bootstrap.min.css";
 class contact extends React.Component
 {
     render(){
+        // top_align_ab
         return(
             <div className="top_align">
-                    <Rb.Card className="shadow top_align_ab">
+                    <Rb.Card className="shadow card_align">
                         <Rb.Card.Body>
                             <section>
-                                <div>test</div>
-                                <div>test</div>
+                                <div className="main_header">
+                                    <h1 className="">Contact</h1>
+                                    <div className="divider"></div>
+                                </div>
+                                <div className="sub_heading mt-4">
+                                    <h2 className="mb-0">Contact Form</h2>
+                                </div>
+                                <Rb.Row>
+                                    <Rb.Col sm={12} md={6} lg={6} xl={6}>
+                                        <Rb.Form className="mt-4">
+                                        <Rb.Form.Group className="mb-4" controlId="formBasicEmail">
+                                            <Rb.Form.Label className="w_color">Full Name</Rb.Form.Label>
+                                            <Rb.Form.Control className="input_align" type="text" placeholder=" Enter Full Name" />
+                                        </Rb.Form.Group>
+                                        <Rb.Form.Group className="mb-4" controlId="formBasicEmail">
+                                            <Rb.Form.Label className="w_color">Email Id</Rb.Form.Label>
+                                            <Rb.Form.Control className="input_align" type="email" placeholder="Enter email Id" />
+                                        </Rb.Form.Group>
+                                        </Rb.Form>
+                                    </Rb.Col>
+                                    <Rb.Col sm={12} md={6} lg={6} xl={6}>
+                                        <Rb.Form className="mt-4">
+                                        <Rb.Form.Group className="mb-4" controlId="formBasicEmail">
+                                            <Rb.Form.Label className="w_color">Company Name</Rb.Form.Label>
+                                            <Rb.Form.Control className="input_align" type="text" placeholder="Enter Company Name" />
+                                        </Rb.Form.Group>
+                                        <Rb.Form.Group className="mb-4" controlId="formBasicEmail">
+                                            <Rb.Form.Label className="w_color">Contact Number</Rb.Form.Label>
+                                            <Rb.Form.Control className="input_align" type="text" placeholder="Enter contact number" />
+                                        </Rb.Form.Group>
+                                        </Rb.Form>
+                                    </Rb.Col>
+                                    <Rb.Col sm={12} md={12} lg={12} xl={12}>
+                                        <Rb.Form className="">
+                                        <Rb.Form.Group className="mb-4" controlId="formBasicEmail">
+                                            <Rb.Form.Label className="w_color">Your Message</Rb.Form.Label>
+                                            <Rb.Form.Control as="textarea" className="input_align" placeholder="Enter Your Message" style={{ height: '100px' }}/>
+                                            
+                                        </Rb.Form.Group>
+                                        
+                                        </Rb.Form>
+                                    </Rb.Col>
+                                    
+                                </Rb.Row>
+                                <div className="text-end">
+                                    <Rb.Button className="btn_align"> Send Message</Rb.Button>
+                                </div>
                                 
                             </section>
                         </Rb.Card.Body>
--- src/pages/ed_ex.js
+++ src/pages/ed_ex.js
@@ -1,22 +1,162 @@
 import React from "react";
 import * as Rb from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import * as Bi from "react-icons/bs";
-class Resume extends React.Component
+import * as Bi from "react-icons/bs";
+class Eduex extends React.Component
 {
     render(){
         return(
             <div className="top_align">
-                    <Rb.Card className="shadow top_align_ab">
+                    <Rb.Card className="shadow card_align">
                         <Rb.Card.Body>
                             <section>
-                                <div>Eduction and Experiences</div>
+                                <div className="main_header">
+                                    <h1 className="">Experience & Education</h1>
+                                    <div className="divider"></div>
+                                </div>
+                                {/* Experience */}
+                                <div className="mt-4">
+                                    <Rb.Row>
+                                        <Rb.Col sm={1} md={1} lg={1} xl={1} xxl={1}>
+                                            <div className="d-flex justify-content-center">
+                                                <span className="box_1 box  text-center">
+                                                    <Bi.BsEnvelope className="profile_icon y_color"/>
+                                                </span>
+                                            </div>
+                                            
+                                        </Rb.Col>
+                                        <Rb.Col sm={11} md={11} lg={11} xl={11} xxl={11} className="my-auto">
+                                        <div className="sub_heading ">
+                                            <h2 className="mb-0">Experience</h2>
+                                        </div>
+                                        </Rb.Col>
+                                    </Rb.Row>
+                                    {/* Saneforce */}
+                                    <Rb.Row>
+                                        <Rb.Col sm={1} md={1} lg={1} xl={1} xxl={1}>
+                                            <div className="position-relative">
+                                                <div className="time_line mx-auto"></div>
+                                                <div className="tl_bcir"></div>
+                                            </div>
+                                            
+                                        </Rb.Col>
+                                        <Rb.Col sm={11} md={11} lg={11} xl={11} xxl={11} className="">
+                                        <div className="sub_heading ">
+                                            <h6 className="mb-0 mt-4 w_color">Saneforce Product Company</h6>
+                                            <p className="y_txt_color fw-4 mt-2">Aug/2022 - Present</p>
+                                            <p className="mb-0">Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit atque corrupti, quos dolores et qvuas molestias exceptur.</p>
+                                        </div>
+                                        </Rb.Col>
+                                    </Rb.Row>
+                                    {/* tag technology */}
+                                    <Rb.Row>
+                                        <Rb.Col sm={1} md={1} lg={1} xl={1} xxl={1}>
+                                            <div className="position-relative">
+                                                <div className="time_line mx-auto"></div>
+                                                <div className="tl_bcir"></div>
+                                            </div>
+                                            
+                                        </Rb.Col>
+                                        <Rb.Col sm={11} md={11} lg={11} xl={11} xxl={11} className="">
+                                        <div className="sub_heading ">
+                                            <h6 className="mb-0 mt-4 w_color">Tag Technology Services</h6>
+                                            <p className="y_txt_color fw-4 mt-2">June/2021 - July/2022</p>
+                                            <p className="mb-0">Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit atque corrupti, quos dolores et qvuas molestias exceptur.</p>
+                                        </div>
+                                        </Rb.Col>
+                                    </Rb.Row>
+                                    {/* Icici */}
+                                    <Rb.Row>
+                                        <Rb.Col sm={1} md={1} lg={1} xl={1} xxl={1}>
+                                            <div className="position-relative">
+                                                <div className="time_line mx-auto"></div>
+                                                <div className="tl_bcir"></div>
+                                            </div>
+                                            
+                                        </Rb.Col>
+                                        <Rb.Col sm={11} md={11} lg={11} xl={11} xxl={11} className="">
+                                        <div className="sub_heading ">
+                                            <h6 className="mb-0 mt-4 w_color">ICICI Prudential Life Insurance</h6>
+                                            <p className="y_txt_color fw-4 mt-2">Aug/2020 - March/2021</p>
+                                            <p className="mb-0">Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit atque corrupti, quos dolores et qvuas molestias exceptur.</p>
+                                        </div>
+                                        </Rb.Col>
+                                    </Rb.Row>
+                                </div>
+                                {/* Education */}
+                                <div className="mt-5">
+                                    <Rb.Row>
+                                        <Rb.Col sm={1} md={1} lg={1} xl={1} xxl={1}>
+                                            <div className="d-flex justify-content-center">
+                                                <span className="box_1 box  text-center">
+                                                    <Bi.BsEnvelope className="profile_icon y_color"/>
+                                                </span>
+                                            </div>
+                                            
+                                        </Rb.Col>
+                                        <Rb.Col sm={11} md={11} lg={11} xl={11} xxl={11} className="my-auto">
+                                        <div className="sub_heading ">
+                                            <h2 className="mb-0">Education</h2>
+                                        </div>
+                                        </Rb.Col>
+                                    </Rb.Row>
+                                    {/* Saneforce */}
+                                    <Rb.Row>
+                                        <Rb.Col sm={1} md={1} lg={1} xl={1} xxl={1}>
+                                            <div className="position-relative">
+                                                <div className="time_line mx-auto"></div>
+                                                <div className="tl_bcir"></div>
+                                            </div>
+                                            
+                                        </Rb.Col>
+                                        <Rb.Col sm={11} md={11} lg={11} xl={11} xxl={11} className="">
+                                        <div className="sub_heading ">
+                                            <h6 className="mb-0 mt-4 w_color">MBA Business Analytics</h6>
+                                            <p className="y_txt_color fw-4 mt-2">2022 - 2024</p>
+                                            <p className="mb-0">Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit atque corrupti, quos dolores et qvuas molestias exceptur.</p>
+                                        </div>
+                                        </Rb.Col>
+                                    </Rb.Row>
+                                    {/* tag technology */}
+                                    <Rb.Row>
+                                        <Rb.Col sm={1} md={1} lg={1} xl={1} xxl={1}>
+                                            <div className="position-relative">
+                                                <div className="time_line mx-auto"></div>
+                                                <div className="tl_bcir"></div>
+                                            </div>
+                                            
+                                        </Rb.Col>
+                                        <Rb.Col sm={11} md={11} lg={11} xl={11} xxl={11} className="">
+                                        <div className="sub_heading ">
+                                            <h6 className="mb-0 mt-4 w_color">Bsc Computer Science</h6>
+                                            <p className="y_txt_color fw-4 mt-2">2017 - 2020</p>
+                                            <p className="mb-0">Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit atque corrupti, quos dolores et qvuas molestias exceptur.</p>
+                                        </div>
+                                        </Rb.Col>
+                                    </Rb.Row>
+                                    {/* Icici */}
+                                    <Rb.Row>
+                                        <Rb.Col sm={1} md={1} lg={1} xl={1} xxl={1}>
+                                            <div className="position-relative">
+                                                <div className="time_line mx-auto"></div>
+                                                <div className="tl_bcir"></div>
+                                            </div>
+                                            
+                                        </Rb.Col>
+                                        <Rb.Col sm={11} md={11} lg={11} xl={11} xxl={11} className="">
+                                        <div className="sub_heading ">
+                                            <h6 className="mb-0 mt-4 w_color">HSC Computer Science</h6>
+                                            <p className="y_txt_color fw-4 mt-2">2016 - 2017</p>
+                                            <p className="mb-0">Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit atque corrupti, quos dolores et qvuas molestias exceptur.</p>
+                                        </div>
+                                        </Rb.Col>
+                                    </Rb.Row>
+                                </div>
                             </section>
                         </Rb.Card.Body>
-                    </Rb.Card>    
-                
-            </div>
+                    </Rb.Card>
+                </div>
         )
     }
 }
-export default Resume
+export default Eduex
