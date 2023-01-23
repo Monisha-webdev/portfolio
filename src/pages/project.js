0bf343d54b16d915c9ab7428483a4385297f0cbf
Monisha <monimaha99@gmail.com>
2023-01-23 18:31:33 +0530

    project page tab work

--- src/pages/project.js
+++ src/pages/project.js
@@ -7,10 +7,26 @@ class Project extends React.Component
     render(){
         return(
             <div className="top_align">
-                    <Rb.Card className="shadow top_align_ab">
+                    <Rb.Card className="shadow card_align">
                         <Rb.Card.Body>
                             <section>
-                                <div>Project</div>
+                                <div className="main_header">
+                                    <h1 className="">Projects</h1>
+                                    <div className="divider"></div>
+                                </div>
+                            </section>
+                            <section>
+                            <Rb.Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3 mt-4">
+                            <Rb.Tab eventKey="home" title="Home">
+                                <h1 className="">Projects 1</h1>
+                            </Rb.Tab>
+                            <Rb.Tab eventKey="profile" title="Profile">
+                                <h1 className="">Projects 2</h1>
+                            </Rb.Tab>
+                            <Rb.Tab eventKey="contact" title="contact">
+                                <h1 className="">Projects 3</h1>
+                            </Rb.Tab>
+                            </Rb.Tabs>
                             </section>
                         </Rb.Card.Body>
                     </Rb.Card>    
