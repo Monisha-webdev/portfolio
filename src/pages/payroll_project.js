0c476aba0a1ac2b08ced4e0411ec4d0908406ec3
monisha-git <monishainfo999@gmail.com>
2023-01-28 20:24:52 +0530

    single project page

--- src/pages/payroll_project.js
+++ src/pages/payroll_project.js
@@ -3,10 +3,32 @@ import * as Rb from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Img_1 from "../assets/img/img1.png"
 import * as Bi from "react-icons/bs";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 class Payroll_pro extends React.Component
 {
+    
     render(){
+        const responsive = {
+            superLargeDesktop: {
+              // the naming can be any, depends on you.
+              breakpoint: { max: 4000, min: 3000 },
+              items: 5
+            },
+            desktop: {
+              breakpoint: { max: 3000, min: 1024 },
+              items: 2
+            },
+            tablet: {
+              breakpoint: { max: 1024, min: 464 },
+              items: 2
+            },
+            mobile: {
+              breakpoint: { max: 464, min: 0 },
+              items: 1
+            }
+          };
         return(
             <div>
                 <Rb.Card className="shadow card_align">
@@ -64,45 +86,25 @@ class Payroll_pro extends React.Component
                                 <p>Scelerisque ipsum pretium augue neque at. Bibendum semper ipsum arcu, nibh blandit facilisi. Quis dictum ornare ultricies porta lectus in metus, purus facilisi. Egestas amet, enim in maecenas ultrices. Ornare donec volutpat enim at eget habitant eleifend. Enim, nisl porttitor egestas etiam a, magna neque.</p>
                             </div>
                             <div className="mt-4">
-                            <Carousel>
-                                <Carousel.Item>
-                                    <img
-                                    className="d-block w-100"
-                                    src={Img_1}
-                                    alt="First slide"
-                                    />
-                                    <Carousel.Caption>
-                                    <h3>First slide label</h3>
-                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img
-                                    className="d-block w-100"
-                                    src="holder.js/800x400?text=Second slide&bg=282c34"
-                                    alt="Second slide"
-                                    />
-
-                                    <Carousel.Caption>
-                                    <h3>Second slide label</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <img
-                                    className="d-block w-100"
-                                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                                    alt="Third slide"
-                                    />
-
-                                    <Carousel.Caption>
-                                    <h3>Third slide label</h3>
-                                    <p>
-                                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                                    </p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                            </Carousel>
+                                <Carousel responsive={responsive}swipeable={false}
+                                    draggable={true}
+                                    showDots={true}
+                                    ssr={true} // means to render carousel on server-side.
+                                    infinite={true}
+                                    // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+                                    autoPlay={false}
+                                    autoPlaySpeed={1000}
+                                    keyBoardControl={true}
+                                    transitionDuration={500}
+                                    containerClass="carousel-container"
+                                    removeArrowOnDeviceType={["tablet", "mobile"]}
+                                    deviceType={this.props.deviceType}
+                                    dotListClass="custom-dot-list-style">
+                                    <Rb.Image fluid src={Img_1} alt="image" className="mx-3" /> 
+                                    <Rb.Image fluid src={Img_1} alt="image" className="mx-3" /> 
+                                    <Rb.Image fluid src={Img_1} alt="image" className="mx-3" /> 
+                                    <Rb.Image fluid src={Img_1} alt="image" className="mx-3" /> 
+                                </Carousel>
                             </div>
                         </section>
                     </Rb.Card.Body>
